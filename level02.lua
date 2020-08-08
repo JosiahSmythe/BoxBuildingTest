16d5accf08b57db0a4b5969481b5485523f4da5b
Josiah Smythe <43389776+JosiahSmythe@users.noreply.github.com>
2020-08-08 09:04:57 -0400

    Added level 2 and shazam image

--- level02.lua
+++ level02.lua
@@ -1,67 +1,69 @@
 -----------------------------------------------------------------------------------------
 --
--- menu.lua
+-- level1.lua
 --
 -----------------------------------------------------------------------------------------
 
 local composer = require( "composer" )
 local scene = composer.newScene()
 
--- include Corona's "widget" library
-local widget = require "widget"
+-- include Corona's "physics" library
+local physics = require "physics"
 
 --------------------------------------------
 
-
 -- forward declarations and other locals
-local playBtn
-
--- 'onRelease' event listener for playBtn
-local function onPlayBtnRelease()
-
-	-- go to level1.lua scene
-	composer.gotoScene( "level1", "fade", 500 )
-
-	return true	-- indicates successful touch
-end
+local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
 
 function scene:create( event )
-	local sceneGroup = self.view
 
 	-- Called when the scene's view does not exist.
 	--
 	-- INSERT code here to initialize the scene
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
-	-- display a background image
-	local background = display.newImageRect( "images/background.jpg", display.actualContentWidth, display.actualContentHeight )
+	local sceneGroup = self.view
+
+	-- We need physics started to add bodies, but we don't want the simulaton
+	-- running until the scene is on the screen.
+	physics.start()
+	physics.pause()
+
+
+	-- create a grey rectangle as the backdrop
+	-- the physical screen will likely be a different shape than our defined content area
+	-- since we are going to position the background from it's top, left corner, draw the
+	-- background at the real top, left corner.
+	local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
 	background.anchorX = 0
 	background.anchorY = 0
-	background.x = 0 + display.screenOriginX
-	background.y = 0 + display.screenOriginY
+	background:setFillColor( .5 )
 
-	-- create/position logo/title image on upper-half of the screen
-	local titleLogo = display.newImageRect( "images/logo.png", 264, 42 )
-	titleLogo.x = display.contentCenterX
-	titleLogo.y = 100
+	-- make a crate (off-screen), position it, and rotate slightly
+	local crate = display.newImageRect( "images/shazam.png", 90, 90 )
+	crate.x, crate.y = 160, -100
+	crate.rotation = 0
 
-	-- create a widget button (which will loads level1.lua on release)
-	playBtn = widget.newButton{
-		label="Play Now Nerd",
-		labelColor = { default={255}, over={128} },
-		default="images/button.png",
-		over="images/button-over.png",
-		width=154, height=40,
-		onRelease = onPlayBtnRelease	-- event listener function
-	}
-	playBtn.x = display.contentCenterX
-	playBtn.y = display.contentHeight - 125
+	-- add physics to the crate
+	physics.addBody( crate, { density=1.0, friction=0.3, bounce=0.0 } )
+
+	-- create a grass object and add physics (with custom shape)
+	local grass = display.newImageRect( "grass.png", screenW, 82 )
+	grass.anchorX = 0
+	grass.anchorY = 1
+	--  draw the grass at the very bottom of the screen
+	grass.x, grass.y = display.screenOriginX, display.actualContentHeight + display.screenOriginY
+
+	-- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
+	local grassShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
+	physics.addBody( grass, "static", { friction=0.3, shape=grassShape } )
 
 	-- all display objects must be inserted into group
 	sceneGroup:insert( background )
-	sceneGroup:insert( titleLogo )
-	sceneGroup:insert( playBtn )
+	sceneGroup:insert( grass )
+	sceneGroup:insert( crate )
 end
+
 
 function scene:show( event )
 	local sceneGroup = self.view
@@ -74,11 +76,13 @@ function scene:show( event )
 		--
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
+		physics.start()
 	end
 end
 
 function scene:hide( event )
 	local sceneGroup = self.view
+
 	local phase = event.phase
 
 	if event.phase == "will" then
@@ -86,23 +90,23 @@ function scene:hide( event )
 		--
 		-- INSERT code here to pause the scene
 		-- e.g. stop timers, stop animation, unload sounds, etc.)
+		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
 	end
+
 end
 
 function scene:destroy( event )
-	local sceneGroup = self.view
 
 	-- Called prior to the removal of scene's "view" (sceneGroup)
 	--
 	-- INSERT code here to cleanup the scene
 	-- e.g. remove display objects, remove touch listeners, save state, etc.
+	local sceneGroup = self.view
 
-	if playBtn then
-		playBtn:removeSelf()	-- widgets must be manually removed
-		playBtn = nil
-	end
+	package.loaded[physics] = nil
+	physics = nil
 end
 
 ---------------------------------------------------------------------------------
