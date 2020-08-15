2523c00873a74a5da3b4b8609cafe7059cd33efd
Josiah Smythe <43389776+JosiahSmythe@users.noreply.github.com>
2020-08-15 15:24:10 -0400

    Update menu.lua
    Changed Level number for menu

--- menu.lua
+++ menu.lua
@@ -20,7 +20,7 @@ local playBtn
 local function onPlayBtnRelease()
 
 	-- go to level1.lua scene
-	composer.gotoScene( "level1", "fade", 500 )
+	composer.gotoScene( "level01", "fade", 500 )
 
 	return true	-- indicates successful touch
 end
